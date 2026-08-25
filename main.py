e0e9d688ee591b6f9ac4760df8f8d77f4f75bbe1
Bahadır Berk Erdemir <bahadirberkerdemir@gmail.com>
2026-08-25 18:05:02 +0300

    Project Finished.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -8,16 +10,18 @@ driver = webdriver.Chrome()
 #driver.maximize_window()
 
 website_dict = {
-                "www.amazon.com.tr": ".nav-input.nav-progressive-attribute",
-                "www.hepsiburada.com":".searchBarContent-UfviL0lUukyp5yKZTi4k",
-                "www.trendyol.com":".search-bar-new-input-active-field"
-                }
+    "https://www.amazon.com.tr/s?k={}": "Amazon",
+    "https://www.hepsiburada.com/ara?q={}": "HepsiBurada",
+    "https://www.trendyol.com/sr?q={}": "Trendyol"
+    }
+
 website_keys = list(website_dict.keys())
+website_values = list(website_dict.values())
 
 sort_class_dict = {
-                    ".a-button-text.a-declarative": '[tabindex="0"]',
+                    ".a-button-text.a-declarative": 'a#s-result-sort-select_1', #value uses tag and id
                    ".horizontalSortingBar-Ce404X9mUYVCRa5bjV4D":".horizontalSortingBar-PkoDOH7UsCwBrQaQx9bn",
-                   ".select-box":'[tabindex="-1"]'
+                   ".select-box":'li#select-option-PRICE_BY_ASC' #value uses tag and id
                    }
 
 sort_keys= list(sort_class_dict.keys())
@@ -35,47 +39,49 @@ product_values = list(product_class_dict.values())
 
 search_input = ''
 
-while search_input.upper() != 'Q':
-    search_input = input("Enter what product you want to search (q to exit): ")
+while True:
+        search_input = input("Enter what product you want to search (q to exit): ")
+        formatted_search = search_input.replace(" ", "+")
 
-    data_dict = {}
-    data_name_list = []
-    data_price_list = []
-
-    if not search_input.upper() == 'Q':
+        data_dict = {}
+        data_name_list = []
+        data_price_list = []
+        if search_input.upper() == 'Q':
+            break
         with open("PriceList.md", "a+") as f:
             f.write(f"# Top 3 cheapest {search_input} \n\n")
+            i = 0
+            for website in website_dict:
+                #try:
+                search_url = website.format(formatted_search)
+                driver.get(search_url)
 
-            for website, search_class in website_dict.items():
-                i=0
-                driver.get(f"https://{website}")
-
-                WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, search_class)))
-
-                search_bar = driver.find_element(By.CSS_SELECTOR, search_class)
-                search_bar.send_keys(search_input)
-                search_bar.send_keys(Keys.ENTER)
-                WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, sort_keys[i])))
+                WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, sort_keys[i])))
 
                 sort_dropdown = driver.find_element(By.CSS_SELECTOR, sort_keys[i])
-                sort_dropdown.click()
-                WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, sort_values[i])))
+                driver.execute_script("arguments[0].click();", sort_dropdown)
+                WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, sort_values[i])))
 
                 asc_prices_label = driver.find_element(By.CSS_SELECTOR, sort_values[i])
-                asc_prices_label.click()
+                driver.execute_script("arguments[0].click();", asc_prices_label)
+                time.sleep(2)
 
                 WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, product_keys[i])))
 
-                data_name_list = driver.find_elements(By.CSS_SELECTOR, f"{product_keys[i]}:nth(-n+3)")
-                data_price_list = driver.find_elements(By.CSS_SELECTOR, f"{product_values[i]}:nth(-n+3)")
+                data_name_list = driver.find_elements(By.CSS_SELECTOR, product_keys[i])[:3]
+                data_price_list = driver.find_elements(By.CSS_SELECTOR, product_values[i])[:3]
 
                 print(f"name_list: {data_name_list}\nprice_list: {data_price_list}")
 
                 for ix in range(len(data_name_list)):
-                    f.write(f"##**Source:** *{website_keys[i]}*\n")
-                    f.write(f"###**Full Description:** {data_name_list[ix]}\n**Price: *{data_price_list[ix]}***\n\n\n")
+                    f.write(f"**Source:** *{website_values[i]}*  \n")
+                    f.write(f"**Full Description:** {data_name_list[ix].text}  \n**Price: *{data_price_list[ix].get_attribute("textContent").strip()}***  \n  \n  \n")
 
                 i+=1
+                """except Exception as e:
+                    i+=1
+                    print(e)
+                    continue"""
 
 
 
@@ -83,3 +89,6 @@ while search_input.upper() != 'Q':
 
 
 
+
+
+time.sleep(10)
